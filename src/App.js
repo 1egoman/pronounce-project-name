9a273ba85d683b39f33c9960f6cc4415497186bb
Ryan Gaus <rsg1egoman@gmail.com>
2017-06-14 08:14:38 -0400

    chore: gettign bunch of data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
 import debounce from 'lodash.debounce';
 import React, { Component } from 'react';
 
+const PRONOUNCIATION_KEY = [
+  {
+    letter: 'a',
+    how: {
+      'a': ['j{a}ck'],
+      'a': ['f{a}ther'],
+      'a': ['{a}round'],
+      'a': ['t{a}ll'],
+      'a': ['c{a}ble'],
+      'a': ['sh{a}re'],
+      'a': ['sen{a}te'],
+    },
+  },
+  {
+    letter: 'e',
+    how: {
+      'i': ['eas{y}'],
+      'iː': ['s{ee}n'],
+    }
+  },
+  {
+    letter: 'i',
+    how: {
+      'i': ['eas{y}'],
+      'ɪ': ['c{i}ty']
+    }
+  },
+  {
+    letter: 'o',
+    how: {
+    }
+  },
+  {
+    letter: 'u',
+    how: {
+    }
+  },
+  {
+    letter: 'y',
+    how: {
+    }
+  }
+];
+
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { showPlayer: false, pronounciation: "" };
+    this.state = {
+      showPlayer: false,
+      pronounciation: "",
+
+      letters: [
+        {}
+      ]
+    };
 
     this.updatePronounciation = debounce(value => {
       this.setState({pronounciation: value});
