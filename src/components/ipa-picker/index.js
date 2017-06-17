71749964e05f5c32fbc789028a3405bf162939d9
Ryan Gaus <rsg1egoman@gmail.com>
2017-06-17 07:33:08 -0400

    code: iterating on ipa picker
    This isn't going so well, hmmm

--- src/components/ipa-picker/index.js
+++ src/components/ipa-picker/index.js
@@ -1,53 +1,17 @@
 import * as React from 'react';
 import './styles.css';
 
-export default class IPAPicker extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidUpdate() {
-    // If if the glyph has a textbox for editing, then focus the textbox
-    if (this.glyphInput) {
-      this.glyphInput.focus();
-    }
-  }
-
-  render() {
-    const {
-      glyph,
-      isEditing,
-      onSelectedGlyph
-    } = this.props;
-
-    if (isEditing) {
-      return <div className="ipa-glyph">
-        <input
-          type="text"
-          value={glyph}
-          ref={input => { this.glyphInput = input; }}
-        />
-
-        <ul className="ipa-glyph-picker ipa-glyph-picker-above">
-          <li>A</li>
-          <li>B</li>
-          <li>C</li>
-        </ul>
-        <ul className="ipa-glyph-picker ipa-glyph-picker-below">
-          <li>A</li>
-          <li>B</li>
-          <li>C</li>
-        </ul>
-      </div>;
-    } else {
-      return <div className="ipa-glyph">
-        <span
-          className="ipa-glyph-static-glyph"
-          onClick={onSelectedGlyph}
-        >
-          {glyph}
-        </span>
-      </div>;
-    }
-  }
+export default function IPAPicker({startPosition, glyph}) {
+  return <div className="ipa-picker-parent" style={{left: startPosition}}>
+    <ul className="ipa-picker ipa-picker-above">
+      <li>A</li>
+      <li>B</li>
+      <li>C</li>
+    </ul>
+    <ul className="ipa-picker ipa-picker-below">
+      <li>A</li>
+      <li>B</li>
+      <li>C</li>
+    </ul>
+  </div>;
 }
--- src/components/ipa-refiner/index.js
+++ src/components/ipa-refiner/index.js
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import './styles.css';
 
+import IPAPicker from '../ipa-picker/index';
+
+const CELL_SIZE = 72; /* px */
+
 export default class IPARefiner extends React.Component {
   constructor(props) {
     super(props);
@@ -12,9 +16,9 @@ export default class IPARefiner extends React.Component {
 
   selectGlyph(e) {
     // TODO: 100 is a bad number her, should be 72. That doesn't work tho...
-    const characterIndex = (e.clientX - 12) / 100;
-
-    console.log(characterIndex);
+    const demarginized = e.clientX - e.target.getBoundingClientRect().left;
+    const characterIndex = Math.floor(e.clientX / CELL_SIZE);
+    this.setState({editingIndex: characterIndex});
   }
 
   render() {
@@ -32,6 +36,11 @@ export default class IPARefiner extends React.Component {
           />
         </div>
       </div>
+
+      {this.state.editingIndex > -1 ? <IPAPicker
+        startPosition={this.state.editingIndex * CELL_SIZE}
+        glyph={glyphs[this.state.editingIndex]}
+      /> : null}
     </div>;
   }
 }
